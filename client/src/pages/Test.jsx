5e2ddc083763e34825c42a803b451d2f6b33a17f
James Smith <james@Jamess-Mac-mini.gateway>
2023-08-28 05:09:03 +0800

    Project Detail Page responsive design

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,76 +1,54 @@
-import {Container, ThemeProvider, Fab} from "@mui/material"
-import {blue} from "@mui/material/colors"
+import { Container, ThemeProvider, Fab, Grid } from '@mui/material';
+import { blue } from '@mui/material/colors';
 import theme from '../theme';
 import FooterNavBar from '../components/FooterNavBar/index';
 import TitleHeader from '../components/TitleHeader/index';
-// import CardMember from '../components/CardMember/index';
-// import CardProjectDetail from '../components/CardProjectDetail/index';
 import CardProjectDetail2 from '../components/CardProjectDetail/p2';
 import CardMember2 from '../components/CardMember/z2';
 import CardMember3 from '../components/CardMember/z3';
-import AddIcon from "@mui/icons-material/Add";
+import AddIcon from '@mui/icons-material/Add';
 
 const ProjectDetail = () => {
-
   return (
-  <ThemeProvider theme={theme}>
-
-    <Container 
-      sx={{ 
-        width: { xs: 400, md: 960, lg: 1280, xl: 1920},
-        bgcolor: blue[50], 
-        height: "90vh",
-        display: "flex",
-        flexDirection: { xs: "column", md: "row"},
-      }}
-    >
-      <main>
-      <TitleHeader />
-
-        <br></br>
-        <br></br>
-        <CardProjectDetail2 />
-        <br></br>
-        <CardMember2 />
-        <br></br>
-        <CardMember3 />
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>        
-        <br></br>
-        <br></br>        
-        <br></br>
-        <br></br>        
-        <br></br>                          
-        <Container 
+    <ThemeProvider theme={theme}>
+      <Container
         sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: 'right',  
+          width: '100%',
+          bgcolor: blue[50],
+          minHeight: '100vh',
+          position: 'relative',
         }}
+      >
+        <main>
+          <TitleHeader />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <CardProjectDetail2 />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <CardMember2 />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <CardMember3 />
+            </Grid>
+          </Grid>
+        </main>
+        <div
+          style={{
+            position: 'absolute',
+            bottom: '16px', 
+            right: '16px',
+            paddingRight: 28,
+          }}
         >
-        {/* <Button 
-        variant="contained"
-        > Add </ Button>
-        <Button 
-        variant="contained"
-        > Cancel </ Button> */}
-        {/* //TODO onClick={handleFabClick} */}
-        <Fab 
-        color="primary"
-        >
+          <Fab color="primary">
             <AddIcon />
-        </Fab>        
-        </Container>
-      </main>
-    </Container>
-    <FooterNavBar />
-  </ThemeProvider>
+          </Fab>
+        </div>
+      </Container>
+      <FooterNavBar />
+    </ThemeProvider>
   );
-
 };
-
 
 export default ProjectDetail;
