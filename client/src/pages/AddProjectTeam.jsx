9a5bc1a3fda3e5904feab95cfed21a4ef05291ce
James Smith <james@Jamess-Mac-mini.gateway>
2023-08-29 20:55:51 +0800

    Responsive Add Project Team Page

--- client/src/pages/AddProjectTeam.jsx
+++ client/src/pages/AddProjectTeam.jsx
@@ -1,90 +1,76 @@
-import {Container, ThemeProvider, Button} from "@mui/material"
-import {blue} from "@mui/material/colors"
+import { Container, ThemeProvider, Button, Grid } from '@mui/material';
+import { blue } from '@mui/material/colors';
 import theme from '../theme';
 import FooterNavBar from '../components/FooterNavBar/index';
 import TitleHeader from '../components/TitleHeader/index';
-// import CardMember from '../components/CardMember/index';
 import CardMember1 from '../components/CardMember/z1';
-// import CardMember2 from '../components/CardMember/z2';
-// import CardMember3 from '../components/CardMember/z3';
-import { Link, useParams, useNavigate,} from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { useMutation, useQuery } from '@apollo/client';
 import { QUERY_PROJECT } from '../utils/queries';
-import {ADD_PROJECTTEAM} from '../utils/mutations';
-
+import { ADD_PROJECTTEAM } from '../utils/mutations';
 
 const AddProjectTeam = () => {
-  const {projectId}= useParams();
+  const { projectId } = useParams();
 
   const { loading, data } = useQuery(QUERY_PROJECT, {
     variables: { projectId: projectId },
   });
-  const project = data?.project || {}
+  const project = data?.project || {};
   const navigate = useNavigate();
 
-  const [addProjectTeam, {error}]= useMutation(ADD_PROJECTTEAM);
+  const [addProjectTeam, { error }] = useMutation(ADD_PROJECTTEAM);
 
-  const handleCardClick = async (teamMemberId)=> {
-
+  const handleCardClick = async (teamMemberId) => {
     try {
-      const {data} = await addProjectTeam({
+      const { data } = await addProjectTeam({
         variables: {
           projectId: projectId,
-          teamMemberId: teamMemberId
-        }
-      })
+          teamMemberId: teamMemberId,
+        },
+      });
     } catch (error) {
       console.error(error);
-    };
+    }
 
     window.location.assign(`/projects/${projectId}`);
-
-  }
+  };
 
   return (
-  <ThemeProvider theme={theme}>
-
-    <Container 
-      sx={{ 
-        width: { xs: 400, md: 960, lg: 1280, xl: 1920},
-        bgcolor: blue[50], 
-        height: "90vh",
-        display: "flex",
-        flexDirection: { xs: "column", md: "row"},
-      }}
-    >
-      <main>
-      <TitleHeader />
-
-        <br></br>
-        <br></br>
-        <CardMember1 
-        project={project}
-        onCardClick={handleCardClick}/>
-                           
-        <Container 
+    <ThemeProvider theme={theme}>
+      <Container
         sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: 'space-between',  
+          width: '100%',
+          bgcolor: blue[50],
+          minHeight: '90vh',
         }}
-        >
-        {/* <Button 
-        variant="contained"
-        > Add </ Button> */}
-        <Link to={`/projects/${projectId}`}>
-        <Button 
-        variant="contained"
-        > Cancel </ Button>
-        </Link>
-        </Container>
-      </main>
-    </Container>
-    <FooterNavBar />
-  </ThemeProvider>
+      >
+        <main>
+          <TitleHeader />
+
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <CardMember1 project={project} onCardClick={handleCardClick} />
+            </Grid>
+          </Grid>
+
+          <Container
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: 6,
+            }}
+          >
+            <Button variant="contained">Add</Button>
+            <Link to={`/projects/${projectId}`}>
+              <Button variant="contained">Cancel</Button>
+            </Link>
+          </Container>
+        </main>
+      </Container>
+      <FooterNavBar />
+    </ThemeProvider>
   );
-
 };
-
 
 export default AddProjectTeam;
--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,51 +1,38 @@
-import { Container, ThemeProvider, Button, Typography, TextField, Grid } from '@mui/material';
+import { Container, ThemeProvider, Button, Grid } from '@mui/material';
 import { blue } from '@mui/material/colors';
 import theme from '../theme';
 import FooterNavBar from '../components/FooterNavBar/index';
 import TitleHeader from '../components/TitleHeader/index';
-import { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link, useNavigate } from 'react-router-dom';
-import { ADD_PROJECT } from '../utils/mutations';
+import CardMember1 from '../components/CardMember/';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useMutation, useQuery } from '@apollo/client';
+import { QUERY_PROJECT } from '../utils/queries';
+import { ADD_PROJECTTEAM } from '../utils/mutations';
 
-const initialForm = {
-  name: '',
-  client: '',
-  budget: 0,
-  sow_title: '',
-  sow_detail: '',
-  background_color: '',
-  image_link: '',
-};
+const AddProjectTeam = () => {
+  const { projectId } = useParams();
 
-const AddProject = () => {
-  const [formState, setFormState] = useState(initialForm);
-  const [addProject, { error, data }] = useMutation(ADD_PROJECT);
+  const { loading, data } = useQuery(QUERY_PROJECT, {
+    variables: { projectId: projectId },
+  });
+  const project = data?.project || {};
   const navigate = useNavigate();
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    const newValue = name === 'budget' ? parseFloat(value) : value;
-    setFormState({
-      ...formState,
-      [name]: newValue,
-    });
-  };
+  const [addProjectTeam, { error }] = useMutation(ADD_PROJECTTEAM);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-
+  const handleCardClick = async (teamMemberId) => {
     try {
-      const { data } = await addProject({
-        variables: { ...formState },
+      const { data } = await addProjectTeam({
+        variables: {
+          projectId: projectId,
+          teamMemberId: teamMemberId,
+        },
       });
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
+      console.error(error);
     }
 
-    setFormState(initialForm);
-    navigate('/projects');
+    window.location.assign(`/projects/${projectId}`);
   };
 
   return (
@@ -61,90 +48,21 @@ const AddProject = () => {
           <TitleHeader />
 
           <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <Typography>Project Name:</Typography>
-              <TextField
-                label="Enter Project Name"
-                name="name"
-                type="text"
-                value={formState.name}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography>Client:</Typography>
-              <TextField
-                label="Enter Client"
-                name="client"
-                type="text"
-                value={formState.client}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography>Budget:</Typography>
-              <TextField
-                label="Enter Budget"
-                name="budget"
-                type="text"
-                value={formState.budget}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography>Image Link:</Typography>
-              <TextField
-                label="Enter Image Link"
-                name="image_link"
-                type="text"
-                value={formState.image_link}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography>SOW Title:</Typography>
-              <TextField
-                label="Enter SOW Title"
-                name="sow_title"
-                type="text"
-                value={formState.sow_title}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography>SOW Detail:</Typography>
-              <TextField
-                label="Enter SOW Detail"
-                name="sow_detail"
-                type="text"
-                value={formState.sow_detail}
-                onChange={handleChange}
-              />
-            </Grid>
             <Grid item xs={12}>
-              <Typography>Background Colour:</Typography>
-              <TextField
-                label="Enter Background Colour"
-                name="background_color"
-                type="text"
-                value={formState.background_color}
-                onChange={handleChange}
-              />
+              <CardMember1 project={project} onCardClick={handleCardClick} />
             </Grid>
           </Grid>
 
           <Container
             sx={{
-              marginTop: 6,
               display: 'flex',
               flexDirection: 'row',
               justifyContent: 'space-between',
+              marginTop: 6,
             }}
           >
-            <Button type="submit" variant="contained">
-              Add
-            </Button>
-            <Link to="/projects">
+            <Button variant="contained">Add</Button>
+            <Link to={`/projects/${projectId}`}>
               <Button variant="contained">Cancel</Button>
             </Link>
           </Container>
@@ -155,4 +73,4 @@ const AddProject = () => {
   );
 };
 
-export default AddProject;
+export default AddProjectTeam;
