5dfb37ae4769c238b5891228bf0c26240d14267e
Paichien <b8350580@gmail.com>
2023-09-01 02:25:56 +0800

    seeds and server.js updated

--- server/server.js
+++ server/server.js
@@ -27,10 +27,20 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+
+    app.get('/assets/avatar/:imageName', (req, res)=>{
+      const imageName = req.params.imageName;
+      res.sendFile(path.join(__dirname, `../client/dist/assets/avatar/${imageName}`))
+    });
+
+    app.get('/assets/logo/:imageName', (req, res)=>{
+      const imageName = req.params.imageName;
+      res.sendFile(path.join(__dirname, `../client/dist/assets/logo/${imageName}`))
     });
   }
 
