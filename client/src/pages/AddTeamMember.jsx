16da02229fd53042feb6a008b0e1e297b47e1723
James Smith <james@Jamess-Mac-mini.gateway>
2023-09-02 03:04:39 +0800

    Redesign Responsive Add Team Member Page

--- client/src/pages/AddTeamMember.jsx
+++ client/src/pages/AddTeamMember.jsx
@@ -1,5 +1,5 @@
-import { Container, ThemeProvider, Button, Typography, TextField } from "@mui/material"
-import { blue } from "@mui/material/colors"
+import { Container, ThemeProvider, Button, Typography, TextField, Grid } from "@mui/material";
+import { blue } from "@mui/material/colors";
 import theme from '../theme';
 import FooterNavBar from '../components/FooterNavBar/index';
 import TitleHeader from '../components/TitleHeader/index';
@@ -57,126 +57,126 @@ const AddTeamMember = () => {
     setFormState(initialForm);
     window.location.assign('/teammembers')
   };
+
   return (
     <ThemeProvider theme={theme}>
-
       <Container
         sx={{
-          width: { xs: 400, md: 960, lg: 1280, xl: 1920 },
+          width: { xs: "100%", md: 960, lg: 1280, xl: 1920 },
           bgcolor: blue[50],
-          height: "90vh",
+          minHeight: "90vh",
           display: "flex",
-          flexDirection: { xs: "column", md: "row" },
+          flexDirection: "column",
         }}
       >
         <form onSubmit={handleFormSubmit}>
           <TitleHeader teamMember={teamMember} title="ADD TEAM MEMBER"/>
-          <Typography
-          >Username:</Typography>
-          <TextField
-            label="Enter Username"
-            name="username"
-            type="text"
-            value={formState.username}
-            onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <Typography
-          >Title:</Typography>
-          <TextField
-            label="Enter Title"
-            name="title"
-            type="text"
-            value={formState.title}
-            onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <Typography
-          >Email:</Typography>
-          <TextField
-            label="Enter Email"
-            name="email"
-            type="email"
-            value={formState.email}
-            onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <Typography
-          >Password:</Typography>
-          <TextField
-            label="Assign Password"
-            name="password"
-            type="password"
-            value={formState.password}
-            onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <Typography
-          >Rate $/Hr:</Typography>
-          <TextField
-            label="Enter Rate $/Hr"
-            name="rate"
-            type="number"
-            value={formState.rate}
-            onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <Typography
-          >Background Colour:</Typography>
-          <TextField
-            name="background_color"
-            type="color"
-            value={formState.background_color}
-            onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <Typography
-          >Image Link:</Typography>
-          <TextField
-            label="Enter Avatar Link"
-            name="image_link"
-            type="text"
-            value={formState.image_link}
-            onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <Container
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: 'space-between',
-            }}
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography>Username:</Typography>
+              <TextField
+                fullWidth
+                label="Enter Username"
+                name="username"
+                type="text"
+                value={formState.username}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>Title:</Typography>
+              <TextField
+                fullWidth
+                label="Enter Title"
+                name="title"
+                type="text"
+                value={formState.title}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>Email:</Typography>
+              <TextField
+                fullWidth
+                label="Enter Email"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>Password:</Typography>
+              <TextField
+                fullWidth
+                label="Assign Password"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>Rate $/Hr:</Typography>
+              <TextField
+                fullWidth
+                label="Enter Rate $/Hr"
+                name="rate"
+                type="number"
+                value={formState.rate}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>Background Colour:</Typography>
+              <TextField
+                fullWidth
+                name="background_color"
+                type="color"
+                value={formState.background_color}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>Image Link:</Typography>
+              <TextField
+                fullWidth
+                label="Enter Avatar Link"
+                name="image_link"
+                type="text"
+                value={formState.image_link}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            justifyContent="space-between"
+            sx={{ mt: 2 }}
           >
-            <Button
-              variant="contained"
-              type="submit"
-            > Add </ Button>
-            <Link
-            to={'/teammembers'}>
-            <Button
-              variant="contained"
-            > Cancel </ Button>
-            </Link>
-          </Container>
+            <Grid item>
+              <Button
+                variant="contained"
+                type="submit"
+              >
+                Add
+              </Button>
+            </Grid>
+            <Grid item>
+              <Link to={'/teammembers'}>
+                <Button
+                  variant="contained"
+                >
+                  Cancel
+                </Button>
+              </Link>
+            </Grid>
+          </Grid>
         </form>
       </Container>
       <FooterNavBar />
     </ThemeProvider>
   );
-
 };
-
 
 export default AddTeamMember;
