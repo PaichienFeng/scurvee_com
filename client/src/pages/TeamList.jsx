3a4adfa4108f84e34310246c0ed62a1809d79422
James Smith <james@Jamess-Mac-mini.gateway>
2023-09-02 02:56:56 +0800

    Redesign Responsive Team List Page

--- client/src/pages/TeamList.jsx
+++ client/src/pages/TeamList.jsx
@@ -1,5 +1,5 @@
-import { Container, ThemeProvider, Fab } from "@mui/material"
-import { blue } from "@mui/material/colors"
+import { Container, ThemeProvider, Fab } from "@mui/material";
+import { blue } from "@mui/material/colors";
 import theme from '../theme';
 import FooterNavBar from '../components/FooterNavBar/index';
 import TitleHeader from '../components/TitleHeader/index';
@@ -12,59 +12,49 @@ import Auth from "../utils/auth";
 
 const TeamList = () => {
   const teamMemberId = Auth.getProfile().authenticatedPerson._id;
-  const {data:teamMemberData} = useQuery(QUERY_TEAMMEMBER,{
-    variables:{teamMemberId: teamMemberId}
-  })
-  const teamMember= teamMemberData?.teamMember||{}
-
+  const { data: teamMemberData } = useQuery(QUERY_TEAMMEMBER, {
+    variables: { teamMemberId: teamMemberId }
+  });
+  const teamMember = teamMemberData?.teamMember || {};
 
   const { loading, data } = useQuery(QUERY_TEAMMEMBERS);
   const teamMembers = data?.teamMembers || []
 
   return (
     <ThemeProvider theme={theme}>
-
       <Container
         sx={{
-          width: { xs: 400, md: 960, lg: 1280, xl: 1920 },
+          width: "100%",
           bgcolor: blue[50],
-          height: "90vh",
+          minHeight: "90vh",
           display: "flex",
-          flexDirection: { xs: "column", md: "row" },
+          flexDirection: "column",
         }}
       >
         <main>
-          <TitleHeader teamMember={teamMember} title="TEAM LIST"/>
-
-          <br></br>
-          <br></br>
-          <CardMemberBin1
-            teamMembers={teamMembers} />
-          <br></br>
-
-          <Container
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: 'right',
-            }}
-          >
-            <Link
-              to={'/teammembers/addteammember'}>
-              <Fab
-                color="primary"
-              >
-                <AddIcon />
-              </Fab>
-            </Link>
-          </Container>
+          <TitleHeader teamMember={teamMember} title="TEAM LIST" />
+          <br />
+          <br />
+          <CardMemberBin1 teamMembers={teamMembers} />
         </main>
+        <Container
+          sx={{
+            mt: "auto",
+            display: "flex",
+            justifyContent: "flex-end",
+            paddingBottom: 2,
+          }}
+        >
+          <Link to={'/teammembers/addteammember'}>
+            <Fab color="primary">
+              <AddIcon />
+            </Fab>
+          </Link>
+        </Container>
       </Container>
       <FooterNavBar />
     </ThemeProvider>
   );
-
 };
-
 
 export default TeamList;
