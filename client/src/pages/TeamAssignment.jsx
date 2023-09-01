c9b7ca19bb2de27dcf1b2575cf7af5c4db5bb4b7
James Smith <james@Jamess-Mac-mini.gateway>
2023-09-02 00:56:04 +0800

    Redesign Responsive Team Assignment Page

--- client/src/pages/TeamAssignment.jsx
+++ client/src/pages/TeamAssignment.jsx
@@ -1,29 +1,36 @@
-import { Container, ThemeProvider, Button, Typography, TextField } from "@mui/material"
-import { blue } from "@mui/material/colors"
-import theme from '../theme';
-import FooterNavBar from '../components/FooterNavBar/index';
-import TitleHeader from '../components/TitleHeader/index';
-import CardProjectTitle2 from '../components/CardProjectTitle/p2';
-import CardMember3 from '../components/CardMember/z3';
+import React, { useState } from "react";
+import {
+  Container,
+  ThemeProvider,
+  Button,
+  Typography,
+  TextField,
+  Box,
+} from "@mui/material";
+import { blue } from "@mui/material/colors";
+import theme from "../theme";
+import FooterNavBar from "../components/FooterNavBar/index";
+import TitleHeader from "../components/TitleHeader/index";
+import CardProjectTitle2 from "../components/CardProjectTitle/p2";
+import CardMember3 from "../components/CardMember/z3";
 import { Link, useParams } from "react-router-dom";
 import { useMutation, useQuery } from "@apollo/client";
-import { ADD_TEAMASSIGNMENT } from '../utils/mutations';
-import { QUERY_PROJECT, QUERY_TEAMMEMBER} from '../utils/queries';
-import { useState } from "react";
+import { ADD_TEAMASSIGNMENT } from "../utils/mutations";
+import { QUERY_PROJECT, QUERY_TEAMMEMBER } from "../utils/queries";
 import Auth from "../utils/auth";
 
 const initialForm = {
-  description: '',
+  description: "",
   plannedDuration: 0,
-  taskDate: '',
-}
+  taskDate: "",
+};
 
 const TeamAssignment = () => {
   const userId = Auth.getProfile().authenticatedPerson._id;
-  const {data:teamMemberData} = useQuery(QUERY_TEAMMEMBER,{
-    variables:{teamMemberId: userId}
-  })
-  const user= teamMemberData?.teamMember||{}
+  const { data: teamMemberData } = useQuery(QUERY_TEAMMEMBER, {
+    variables: { teamMemberId: userId },
+  });
+  const user = teamMemberData?.teamMember || {};
 
   const { teamMemberId } = useParams();
   const { projectId } = useParams();
@@ -32,7 +39,8 @@ const TeamAssignment = () => {
   });
   const project = projectData?.project || {};
 
-  const teamMember = project.teamMembers?.find(member=>member._id=== teamMemberId) || {}
+  const teamMember =
+    project.teamMembers?.find((member) => member._id === teamMemberId) || {};
 
   const [formState, setFormState] = useState(initialForm);
   const [addTeamAssignment, { error }] = useMutation(ADD_TEAMASSIGNMENT);
@@ -51,115 +59,105 @@ const TeamAssignment = () => {
     try {
       const { data: AssignmentData } = await addTeamAssignment({
         variables: {
-          "teamMemberId": teamMemberId,
-          "projectId":projectId,
-          "description": formState.description,
-          "plannedDuration": formState.plannedDuration,
-          "taskDate": formState.taskDate
+          teamMemberId: teamMemberId,
+          projectId: projectId,
+          description: formState.description,
+          plannedDuration: formState.plannedDuration,
+          taskDate: formState.taskDate,
         },
       });
 
       if (AssignmentData) {
         if (AssignmentData.addTeamAssignment !== null) {
           alert("You have successfully assigned the task!");
-          window.location.assign('/projects');
-
+          window.location.assign("/projects");
         } else {
-          alert("No task found")
+          alert("No task found");
         }
       }
     } catch (error) {
       console.error(error);
-      alert("Error, duplicate task!")
-    };
+      alert("Error, duplicate task!");
+    }
 
     setFormState(initialForm);
-    
   };
+
   return (
     <ThemeProvider theme={theme}>
-
       <Container
         sx={{
-          width: { xs: 400, md: 960, lg: 1280, xl: 1920 },
+          position: "relative",
+          width: { xs: "100%", md: 960, lg: 1280, xl: 1920 },
           bgcolor: blue[50],
-          height: "90vh",
+          minHeight: "100vh",
           display: "flex",
           flexDirection: { xs: "column", md: "row" },
         }}
       >
-        <form onSubmit={handleFormSubmit}>
-          <TitleHeader teamMember={user} title="TEAM ASSIGNMENT"/>
+        <form onSubmit={handleFormSubmit} style={{ width: "100%" }}>
+          <TitleHeader teamMember={user} title="TEAM ASSIGNMENT" />
 
+          <Box my={2}>
+            <CardProjectTitle2 project={project} />
+          </Box>
+          <Box my={2}>
+            <CardMember3 teamMember={teamMember} />
+          </Box>
 
-          <br></br>
-          <CardProjectTitle2 project={project} />
-          <br></br>
-          <CardMember3 teamMember={teamMember} />
-          <br></br>
-          <Typography
-          >Date:</Typography>
+          <Typography>Date:</Typography>
           <TextField
             name="taskDate"
             type="date"
-            // value={formState.task_date}
             onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <Typography
-          >Task:</Typography>
+            fullWidth
+          />
+
+          <Typography>Task:</Typography>
           <TextField
             label="Enter Task"
             name="description"
             type="text"
-            // value={formState.description}
             onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <Typography
-          >Duration:</Typography>
+            fullWidth
+          />
+
+          <Typography>Duration:</Typography>
           <TextField
             label="Enter Duration"
             name="plannedDuration"
             type="number"
-            // value={formState.plannedDuration}
             onChange={handleChange}
-          ></TextField>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <Container
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: 'space-between',
-            }}
-          >
-            <Button
-              type="submit"
-              variant="contained"
-            > Add </ Button>
-            <Link
-              to={`/projects/${projectId}`}>
-              <Button
-                variant="contained"
-              > Cancel </ Button>
-            </Link>
-          </Container>
+            fullWidth
+          />
+
+          <Box mt={2} mb={2}>
+            <Container
+              sx={{
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                right: 0,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                padding: "16px",
+                backgroundColor: blue[50],
+              }}
+            >
+              <Button type="submit" variant="contained">
+                Add
+              </Button>
+              <Link to={`/projects/${projectId}`}>
+                <Button variant="contained">Cancel</Button>
+              </Link>
+            </Container>
+          </Box>
         </form>
       </Container>
       <FooterNavBar />
     </ThemeProvider>
   );
-
 };
-
 
 export default TeamAssignment;
